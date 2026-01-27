fdb1456445cbac22c5894dbc1d7be5028f01705a
侯鹏周 <houpengzhou@haigangcheng.onaliyun.com>
2026-01-27 10:33:46 +0800

    Refactor sharp.js to simplify

--- node-functions/koa/lib/sharp/lib/sharp.js
+++ node-functions/koa/lib/sharp/lib/sharp.js
@@ -3,21 +3,8 @@ const runtimePlatform = runtimePlatformArch();
 const path = require('path');
 const fs = require('fs');
 
-
-// 尝试多个可能的路径（从 public 目录加载）
 const paths = [
-  // 从 public 目录加载（EdgeOne Pages 可能会将 public 目录的内容复制到运行时环境）
-  path.join(process.cwd(), 'public/img/sharp-' + runtimePlatform + '/lib/sharp-' + runtimePlatform + '.node'),
   path.join(process.cwd(), 'img/sharp-' + runtimePlatform + '/lib/sharp-' + runtimePlatform + '.node'),
-  // 相对路径（从 sharp.js 的位置）
-  path.join(__dirname, '../../../../public/img/sharp-' + runtimePlatform + '/lib/sharp-' + runtimePlatform + '.node'),
-  path.join(__dirname, '../img/sharp-' + runtimePlatform + '/lib/sharp-' + runtimePlatform + '.node'),
-  // 如果当前平台不可用，尝试使用 linux-x64（生产环境）
-  ...(runtimePlatform !== 'linux-x64' ? [
-    path.join(process.cwd(), 'public/img/sharp-linux-x64/lib/sharp-linux-x64.node'),
-    path.join(process.cwd(), 'img/sharp-linux-x64/lib/sharp-linux-x64.node'),
-    path.join(__dirname, '../../../../public/img/sharp-linux-x64/lib/sharp-linux-x64.node'),
-  ] : []),
 ];
 
 let sharp;
